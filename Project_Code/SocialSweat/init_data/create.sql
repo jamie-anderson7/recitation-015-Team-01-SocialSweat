94f4540310c26ca5dd44e1656616963c7cd6bc39
Alex Philipsen <alph9335@colorado.edu>
2023-04-20 01:44:16 -0600

    Committing because create.sql is causing errors with stash pop

--- Project_Code/SocialSweat/init_data/create.sql
+++ Project_Code/SocialSweat/init_data/create.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -24,3 +25,53 @@ CREATE TABLE IF NOT EXISTS workouts (
 
 /* Creating a valid user for lab 11 */
 INSERT INTO users (username, password, sweats) VALUES ('username', 'password', 0);
+=======
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    sweats INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    user_id INT NOT NULL,
+    firend_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_to_workouts (
+    user_id INT NOT NULL,
+    workout_id INT
+);
+
+CREATE TABLE IF NOT EXISTS workouts (
+    workout_id SERIAL PRIMARY KEY,
+    name TEXT,
+    level INT,
+    sweats INT
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    sweats INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    user_id INT NOT NULL,
+    firend_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_to_workouts (
+    user_id INT NOT NULL,
+    workout_id INT
+);
+
+CREATE TABLE IF NOT EXISTS workouts (
+    workout_id SERIAL PRIMARY KEY,
+    name TEXT,
+    level INT,
+    sweats INT
+);
+
+/* Creating a valid user for lab 11 */
+INSERT INTO users (user_id, username, password, sweats) VALUES (1, 'username', 'password', 0);
+INSERT INTO users (user_id, username, password, sweats) VALUES (1, 'username', 'password', 0);
+>>>>>>> Stashed changes
